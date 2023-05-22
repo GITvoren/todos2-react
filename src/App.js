189caf83e87f7a75b463ea64a857d366336b85f2
GITvoren <tejuco.voren@gmail.com>
2023-05-23 05:54:05 +0800

    ui done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import add from './assets/add.png'
+import edit from './assets/edit.png'
+import trash from './assets/trash.png'
+import check from './assets/check.png'
 
-function App() {
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="todo">
+            <h2>To Do's App</h2>
+            <form>
+              <input type="text" />
+              <img src={add} className="icon" alt="add" />
+            </form>
+            <hr />
+            <div className="task-list">
+                <div className="task">
+                    <p>description of tasn of tasn of tasn of task</p>
+                    <div className="icon-row">
+                      <img src={check} className="icon2" alt="check" />
+                      <img src={edit} className="icon2" alt="edit" />
+                      <img src={trash} className="icon2" alt="delete" />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
